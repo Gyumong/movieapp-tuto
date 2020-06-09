31ce72fef1f109b8989ea57522c9e35a1d1537d0
Gyumong <rlaalsrb3559@naver.com>
2020-06-09 15:07:31 +0900

    LandingPage에서 MainImage 적용

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
-export const IMAGE_BASE_URL = 'https://image/tmdb.org/t/';
+export const API_KEY ='4387088a45d1c280db191a99ce71c2af';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
